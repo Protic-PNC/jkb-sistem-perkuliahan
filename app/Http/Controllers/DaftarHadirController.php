68506ba7739d56703b590275d3ae839bcd56674e
adisalaras <adisalaras41@gmail.com>
2024-07-29 23:15:43 +0700

    feat: make file model, migration, controller and resource

---
+++ app/Http/Controllers/DaftarHadirController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DaftarHadir;
+use Illuminate\Http\Request;
+
+class DaftarHadirController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DaftarHadir $daftarHadir)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DaftarHadir $daftarHadir)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, DaftarHadir $daftarHadir)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DaftarHadir $daftarHadir)
+    {
+        //
+    }
+}
